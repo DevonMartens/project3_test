c055231f6764015ca2a52aa024024769338f028f
DevonMartens <dmmartens91@gmail.com>
2020-01-21 15:27:30 -0500

    validation added to routes user.js needs to match models.js bcrypt added to models.js to hash passwords

--- routes/api/user.js
+++ routes/api/user.js
@@ -19,9 +19,37 @@ router.get('/register', function(req, res){
 res.render('register');
 });
 
-  //register
+  //login
 
   router.get('/login', function(req, res){
     res.render('login');
     }); 
+
+//register user 
+
+router.post ('/register',, function(req, res){
+  name = req.body.name;
+  email = req.body.email;
+  username = req.body.username;
+  password = req.body.password;
+  password2 = req.body.password2;
+  }); 
+
+  //validation all fields required
+req.checkBody('name', 'Name is Required').notEmpty(); 
+req.checkBody('email', 'Email is Required').isEmail (); 
+req.checkBody('username', 'User Name is Required').notEmpty(); 
+req.checkBody('password', 'Password is Required').notEmpty(); 
+req.checkBody('password2', 'Passwords do not match.').equals(req.body.password); 
+
+//render form is errors
+var errors = req.validationErrors();
+if(errors){
+res.render('users/register', {
+errors:  errors 
+});
+}
+else{
+var newUser =
+}
 module.exports = router;  
